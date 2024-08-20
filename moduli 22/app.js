19a0b7302994fc25e9c0ac1dbfafc53dba59385c
arbenitasv <arbenita.sverka5@gmail.com>
2024-08-20 18:05:20 +0200

    moduli 22

---
+++ moduli 22/app.js
@@ -0,0 +1,77 @@
+console.log("Good Job Guys!")
+var x = 5
+//var 124=10 sbon nr
+//var %12=10 sbon
+var a = 50 + 50
+console.log(a)
+
+var b=10
+var c=20
+var d=b+c
+console.log(d)
+
+var g=c-b
+console.log(g)
+
+var k=10
+var l=4
+var m=k%l
+console.log(m)
+
+var n=99
+n++ //postfix
+console.log(n)
+
+var o=101;
+o--
+console.log(0)
+
+var p=5
+var q=2
+var h=p**q //5**2
+console.log(h)
+
+var e=15
+e+=5 //15+5
+console.log(e)
+
+e-=5
+console.log(e) //->15
+
+var  u=10
+u*=3    /30
+console.log(u)
+
+u/=2   /15
+console.log(u)
+////////
+var t=5
+var i=5
+console.log(t==i) ///5==5 true
+
+var n1=6
+var n2=7
+console.log(n1!=n2)
+
+console.log(n1<n2)  
+console.log(n1>n2)
+
+var numri=123
+var tekst ="1233"
+console.log(numri===tekst)
+
+var firstName= "Digital"
+var lastName= "School"
+var fullName= firstName+ " " + lastName
+console.log(`Hello from $(school)`)
+
+//and
+var age=24
+console.log(age >=16 && age <=65)
+var num1=5
+var num2=6
+console.log(num1==5 || num2==5)
+
+var num3=9
+var num4=10
+console.log(!(num3==num4))
