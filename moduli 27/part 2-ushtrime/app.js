6049752d46c1a70b06df7e05a4e011ca44841c2d
arbenitasv <arbenita.sverka5@gmail.com>
2024-08-29 16:20:44 +0200

    moduli 27/ushtrime

---
+++ moduli 27/part 2-ushtrime/app.js
@@ -0,0 +1,21 @@
+//do while loop
+
+var i = 0;
+do{
+    i++
+    console.log(i)
+
+}while(i<5)
+console.log("__________________")
+//while loop
+
+var j=0;
+while(j<5){
+    j++
+    console.log(j)
+}
+console.log("__________________")
+//for loop
+for(var i=0; i<6; i++){
+    console.log("value of is is: "+i)
+}
